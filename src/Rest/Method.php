979aba09f5d1813c6aa8483e82f2151e34ba7956
Patrick Gingras <775.pg.12@gmail.com>
2018-11-29 06:09:23 -0500

    add http method constants

---
+++ src/Rest/Method.php
@@ -0,0 +1,14 @@
+<?php
+namespace p7g\Discord\Rest;
+
+final class Method {
+  public const CONNECT = 'CONNECT';
+  public const DELETE = 'DELETE';
+  public const GET = 'GET';
+  public const HEAD = 'HEAD';
+  public const OPTIONS = 'OPTIONS';
+  public const PATCH = 'PATCH';
+  public const POST = 'POST';
+  public const PUT = 'PUT';
+  public const TRACE = 'TRACE';
+}

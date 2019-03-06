adba1637e17b9ecab9f766a7087e6a390bb7faab
Patrick Gingras <775.pg.12@gmail.com>
2019-03-06 16:46:15 -0500

    added some documentation

--- src/Utility/cast.php
+++ src/Utility/cast.php
@@ -1,6 +1,14 @@
 <?php
 namespace p7g\Discord\Utility;
 
+/**
+ * Convert an object into a class instance by assigning all the object
+ * properties to the same property on the class;
+ *
+ * @param string $className The name of the class to cast to
+ * @param object $obj       The object to cast into the class
+ * @return object An instance of the class passed in as $className
+ */
 function cast(string $className, object $obj) {
   $class = new $className;
   foreach ($obj as $key => $value) {
